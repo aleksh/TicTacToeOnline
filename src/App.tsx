00f887f2164bcd25c4793b1f2cafa9ee7941b2de
Oleksii <waved.cat@gmail.com>
2019-05-24 19:37:44 +0300

    added user info to db users after signIn

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Login from "./components/Login/Login";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { auth, providerFacebook } from "./init/firebaseConfig";
+import { auth, providerFacebook, fb } from "./init/firebaseConfig";
 
 import { userActions } from "./bus/user/actions";
 import VOUser from "./VO/VOUser";
@@ -19,41 +19,80 @@ interface IAppState {}
 
 class App extends React.Component<IAppProps, IAppState> {
 	componentDidMount = () => {
-		const { actions } = this.props;
 		//auth.signOut();
 		// need add is Loading state for user global
+
 		auth.onAuthStateChanged((user: any) => {
 			console.log("Check if user logged in");
+			console.log(user);
 			if (user) {
-				const pUser: VOUser = new VOUser(
-					user.uid,
-					user.displayName,
-					user.photoURL
-				);
-
-				actions.setUser(pUser);
+				this._checkIfUserExistIDB(user);
 			}
 		});
 	};
 
-	_handlerLogin = () => {
-		const { actions } = this.props;
-		console.log("click Login");
-		auth.signInWithPopup(providerFacebook)
-			.then(result => {
-				const user: any = result.user;
-				console.log(user);
-				const pUser: VOUser = new VOUser(
-					user.uid,
-					user.displayName,
-					user.photoURL
-				);
+	_checkIfUserExistIDB = (user: any) => {
+		console.log("_checkIfUserExistIDB");
+		console.log(user);
 
-				actions.setUser(pUser);
-			})
-			.catch(error => {
-				console.log("Error Login");
-			});
+		if (user) {
+			if (user) {
+				fb.database()
+					.ref("users/" + user.uid)
+					.once("value")
+					.then(snapshot => {
+						var pIsUser = snapshot.val();
+						if (pIsUser) {
+							console.log("_setUpUser 3333333333333333333");
+							this._setUpUser(pIsUser);
+							console.log("User exist");
+						} else {
+							console.log("User Null");
+							fb.database()
+								.ref("users/" + user.uid)
+								.set(
+									{
+										uid: user.uid,
+										displayName: user.displayName,
+										photoURL: user.photoURL
+									},
+									error => {
+										if (error) {
+											// The write failed...
+											console.log(
+												"error Save User Data" + error
+											);
+										} else {
+											// Data saved successfully!
+											console.log(
+												"data new User Data Saved"
+											);
+											console.log(
+												"_setUpUser 222222222222222"
+											);
+											this._setUpUser(user);
+										}
+									}
+								);
+						}
+					});
+			}
+		}
+	};
+
+	_setUpUser = (user: any) => {
+		const { actions } = this.props;
+
+		const pUser: VOUser = new VOUser(
+			user.uid,
+			user.displayName,
+			user.photoURL
+		);
+		actions.setUser(pUser);
+	};
+
+	_handlerLogin = () => {		
+		auth.signInWithPopup(providerFacebook);
 	};
 
 	public render() {
